76ab588bad77b1278e095ce7172b285e688c958b
msglover46 <msglover46@gmail.com>
2020-12-19 02:15:52 +0900

    9. タイムライン画面まで作成。

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 三輪駿 on 2020/12/18.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
